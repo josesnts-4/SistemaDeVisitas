d2b41b5b0038243c660faa323aa339852241377e
Victor Manuel <victormanuelgama21@gmail.com>
2025-09-19 20:36:13 -0300

    Update HomeController.java

--- src/main/java/com/inter/controller/HomeController.java
+++ src/main/java/com/inter/controller/HomeController.java
@@ -1,3 +1,4 @@
+// src/main/java/com/inter/SistemaDeVisitas/controller/HomeController.java
 package com.inter.SistemaDeVisitas.controller;
 
 import org.springframework.stereotype.Controller;
@@ -5,12 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String index() { return "login"; }
-
-    @GetMapping("/home")
-    public String home() { return "home"; }
-
-    @GetMapping("/login")
-    public String login() { return "login"; }
+  @GetMapping("/")      public String root()  { return "login"; }
+  @GetMapping("/login") public String login() { return "login"; }
+  @GetMapping("/home")  public String home()  { return "home"; }
 }
