7bd99ed4cfac81fa7f15ee37e3290c671551a598
Victor Manuel <victormanuelgama21@gmail.com>
2025-09-21 23:26:37 -0300

    Update SistemaDeVisitasApplication.java

--- src/main/java/com/inter/SistemaDeVisitas/SistemaDeVisitasApplication.java
+++ src/main/java/com/inter/SistemaDeVisitas/SistemaDeVisitasApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SistemaDeVisitasApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SistemaDeVisitasApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SistemaDeVisitasApplication.class, args);
+  }
 }
